d54c7e2720606c33cbe4989466d6b70ef5502b86
PPakSang <ppm5377@naver.com>
2022-07-25 15:13:31 +0900

    [#1] ESTABLISH : 프로젝트 환경 설정 외부 라이브러리 추가, 테스트 구동 완료

---
+++ src/test/java/com/knud4/an/AnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knud4.an;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
