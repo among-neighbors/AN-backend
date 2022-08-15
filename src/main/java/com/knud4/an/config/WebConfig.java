8c1f2457d7662ad53f341051841149b26c8078e8
PPakSang <ppm5377@naver.com>
2022-08-15 18:17:25 +0900

    Refactor: Account/Profile 필터 등록 및 CORS OPTION 요청 허용 security filter 가 CORS filter 를 먼저 거쳐서 비인가 요청으로 판단하는 상황을 피합니다.

--- src/main/java/com/knud4/an/config/WebConfig.java
+++ src/main/java/com/knud4/an/config/WebConfig.java
@@ -1,16 +1,37 @@
 package com.knud4.an.config;
 
+import com.knud4.an.auth.util.AccountTokenInterceptor;
+import com.knud4.an.auth.util.ProfileTokenInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final AccountTokenInterceptor accountTokenInterceptor;
+    private final ProfileTokenInterceptor profileTokenInterceptor;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "DELETE", "PUT");
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(accountTokenInterceptor).addPathPatterns(
+                "/api/v1/account/**", "/api/v1/auth/profile/**"
+        );
+
+        registry.addInterceptor(profileTokenInterceptor).addPathPatterns(
+                "/api/v1/profile/**"
+        );
     }
 }
