d1143d9d790a60e545c55cae374445986354977e
황아영 <ayoganatanatda@naver.com>
2022-08-18 20:32:48 +0900

    Feat: Comment Repository 추가

---
+++ src/main/java/com/knud4/an/comment/repository/CommunityCommentRepository.java
@@ -0,0 +1,33 @@
+package com.knud4.an.comment.repository;
+
+import com.knud4.an.comment.entity.CommunityComment;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class CommunityCommentRepository {
+
+    private final EntityManager em;
+
+    public void save(CommunityComment comment) {
+        em.persist(comment);
+    }
+
+    public List<CommunityComment> findAllByCommunityId(Long communityId, int page, int count) {
+        return em.createQuery("select c from CommunityComment c where c.community = :communityId", CommunityComment.class)
+                .setParameter("communityId", communityId)
+                .setFirstResult((page-1)*count)
+                .setMaxResults(count)
+                .getResultList();
+    }
+
+    public Long findCountByCommunityId(Long communityId) {
+        return em.createQuery("select count(c) from CommunityComment c where c.community = :communityId", Long.class)
+                .setParameter("communityId", communityId)
+                .getSingleResult();
+    }
+}
