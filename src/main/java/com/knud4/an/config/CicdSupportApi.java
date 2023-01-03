f2e1e7962e951e115fe4a9a8ccc9c83ee31fbffc
PPakSang <sanghyun-dev@naver.com>
2023-01-03 15:30:53 +0900

    Feat: Health Check Dependency 추가

--- src/main/java/com/knud4/an/config/CicdSupportApi.java
+++ src/main/java/com/knud4/an/config/CicdSupportApi.java
@@ -3,6 +3,7 @@ package com.knud4.an.config;
 import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CicdSupportApi {
     private final Environment env;
 
+    @GetMapping("/status/profile")
     public String getProfileName() {
         return Arrays.stream(env.getActiveProfiles()).findFirst().orElse("");
     }
