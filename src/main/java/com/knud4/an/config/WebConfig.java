a2123356a94fea42bb95314ddbd760b37e3cecf7
PPakSang <sanghyun-dev@naver.com>
2023-01-03 12:59:50 +0900

    Refactor: 외부 접속 허용

--- src/main/java/com/knud4/an/config/WebConfig.java
+++ src/main/java/com/knud4/an/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "DELETE", "PUT")
                 .allowedHeaders("*")
                 .exposedHeaders("*")
